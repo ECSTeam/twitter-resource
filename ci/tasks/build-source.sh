ab5da8ac27a9474b3c07efb381276fb04110561b
Josh Ghiloni <jghiloni@ecsteam.com>
2016-04-16 18:54:53 -0600

    Go back to original directory for docker build

--- ci/tasks/build-source.sh
+++ ci/tasks/build-source.sh
@@ -10,7 +10,9 @@ set -e
 
 export GOPATH=`pwd`/gopath:${basedir}/cmd/out/Godeps/_workspace
 
+origbase=`pwd`
 cd ${basedir}
 go build -o ${build_dir}/check ./cmd/check
 go build -o ${build_dir}/in ./cmd/in
 go build -o ${build_dir}/out ./cmd/out
+cd ${origbase}
