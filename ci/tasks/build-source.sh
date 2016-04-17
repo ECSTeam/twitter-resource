9243457a72bad39dd7a2de6494a4ae77294038d0
Josh Ghiloni <jghiloni@ecsteam.com>
2016-04-16 19:10:14 -0600

    Use better task output directories

--- ci/tasks/build-source.sh
+++ ci/tasks/build-source.sh
@@ -2,9 +2,9 @@
 set -x
 
 basedir=`pwd`/gopath/src/github.com/ECSTeam/twitter-resource
-build_dir=`pwd`/assets
+build_dir=`pwd`/build-output
 
-mkdir ${build_dir} > /dev/null
+mkdir -p ${build_dir} > /dev/null
 
 set -e
 
@@ -12,7 +12,9 @@ export GOPATH=`pwd`/gopath:${basedir}/cmd/out/Godeps/_workspace
 
 origbase=`pwd`
 cd ${basedir}
-go build -o ${build_dir}/check ./cmd/check
-go build -o ${build_dir}/in ./cmd/in
-go build -o ${build_dir}/out ./cmd/out
+go build -o ${build_dir}/assets/check ./cmd/check
+go build -o ${build_dir}/assets/in ./cmd/in
+go build -o ${build_dir}/assets/out ./cmd/out
 cd ${origbase}
+
+cp -a Dockerfile ${build_dir}
